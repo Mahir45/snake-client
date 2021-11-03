7f82053610747c05d7e2df9228c4abd15dece1fa
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-03 19:13:47 -0400

    create functoin

--- play.js
+++ play.js
@@ -1,8 +1,24 @@
 const  connect  = require("./client");
 const net = require("net");
+const handleUserInput = function (key) {
+  console.log("Leave") 
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+};
 // establishes a connection with the game server
-
+setupInput()
 
 console.log("Connecting ...");
 connect();
