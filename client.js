c89bdd1e9df81231c7885911c717feaa5dee4f9a
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-03 21:36:58 -0400

    Constants add

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require("net");
-const connect = function () {
+const { IP, PORT } = require("./constants");
+const connect = function (PORT, IP) {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541// PORT number here,
